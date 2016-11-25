19141e1dd3f39f8e072ee146a989fda5faa6daad
YongJS <dunett@nate.com>
2016-11-25 15:25:39 +0900

    문의하기, 공지사항 완

---
+++ server/src/model/menu.js
@@ -0,0 +1,71 @@
+const pool = require('./dbConnection');
+
+class Menu{
+
+}
+
+Menu.showNotice = function(cb){
+    pool.getConnection(function(err, conn){
+        const sql = 'SELECT title, description, contents, date FROM Notice order by date desc';
+        conn.query(sql, function(err, results){
+            if(err){
+                err.code = 500;
+                conn.release();
+                return cb(err, {msg: 'Failure'});
+            }
+            return cb(null, {msg: 'Success', list: results});
+        })
+    })
+}
+
+Menu.addNotice = function(){
+
+}
+
+Menu.showBoard = function(id, cb){
+    pool.getConnection(function(err, conn){
+        const sql = 'SELECT u.name, b.question, b.answer, b.q_date, b.a_date FROM Board b, User u WHERE b.u_id = ? AND b.u_id = u.u_id order by b.q_date desc';
+        conn.query(sql, id, function(err, results){
+            if(err){
+                err.code = 500;
+                conn.release();
+                return cb(err, {msg: 'Failure'});
+            }
+            return cb(null, {msg: 'Success', list: results});
+        })
+    })
+}
+
+Menu.addQuestion = function(info, cb){
+    pool.getConnection(function(err, conn){
+        const sql = 'INSERT INTO Board SET ?';
+        conn.query(sql, info, (err, result) =>{
+            if(err){
+                err.code = 500;
+                conn.release();
+                return cb(err, {msg: 'Failure'});
+            }
+            conn.commit();
+            conn.release();
+            return cb(null, {msg : "Success"});
+        });
+    })
+}
+
+Menu.addAnswer = function(ans, id, cb){
+    pool.getConnection(function(err, conn){
+        const sql = 'UPDATE Board SET answer = ?, a_date = now() WHERE b_id = ?';
+        conn.query(sql, [ans, id], (err, result) =>{
+            if(err){
+                err.code = 500;
+                conn.release();
+                return cb(err, {msg: 'Failure'});
+            }
+            conn.commit();
+            conn.release();
+            return cb(null, {msg : "Success"});
+        })
+    })
+}
+
+module.exports = Menu;
