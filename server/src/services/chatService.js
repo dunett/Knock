adf4d27b76371d581996b4d8b6ce315e062a4384
lasen7 <lasen7@naver.com>
2016-11-29 00:56:04 +0900

    Updating chat service

--- server/src/services/chatService.js
+++ server/src/services/chatService.js
@@ -10,62 +10,32 @@ const Send_Message = 'sendMessage';
 const Join_Room = 'joinRoom';
 
 const redisInfo = {
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 6379,
 };
 
-const pub = require('redis').createClient(redisInfo.port, redisInfo.host, { return_buffers: true, auth_pass: '' });
-const sub = require('redis').createClient(redisInfo.port, redisInfo.host, { return_buffers: true, auth_pass: '' });
+let rooms = [];
+
+const pub = require('redis').createClient();
+const sub = require('redis').createClient();
+const client = require('redis').createClient();
 
 module.exports = function (http) {
   var io = socketio(http);
   io.adapter(redis({
     host: redisInfo.host,
     port: redisInfo.port,
-    pubClient: pub,
-    subClient: sub,
   }));
+
+  // initialize all key in redis
+  client.flushdb((err, succeeded) => {
+    console.log(succeeded);
+  });
 
   io.on('connection', socket => {
     // room id
     let room = '';
     console.log('a user connnted');
-
-    /**
-     * Send message
-     *  Params:
-     *   - data: {from: 'FromAlias', to: 'ToAlias', message: 'messages'}  
-     */
-    socket.on(Send_Message, (data) => {
-      // Get client count of current room
-      let clients = io.sockets.adapter.rooms[room];
-      if (!clients || room == '') {
-        // if room does not exist, return
-        return;
-      }
-      console.log('room client count: ', clients);
-
-      // 1. Save chat message into mongodb
-      Chat.saveChatMessage({
-        r_id: room,
-        from: data.from,
-        to: data.to,
-        message: data.message
-      }, (err, result) => {
-        if (err) {
-          console.error('saveChatMessage error:', err.stack);
-          socket.emit('foo', 'Server error');
-          return;
-        }
-
-        // if (clients && clients.length == 1) {
-        //   // TODO: 2. push message
-        // } else {
-          // 2. Sending to all clients in room(channel) except sender
-          socket.broadcast.to(room).emit(Send_Message, data.message);
-        // }
-      });
-    });
 
     /**
      * Join the room
@@ -74,15 +44,73 @@ module.exports = function (http) {
      */
     socket.on(Join_Room, (data) => {
       room = data.room;
-
       socket.join(data.room);
 
-      console.log('join the room: ', room);
+      if (rooms[room] == undefined) {
+        rooms[room] = [];
+      }
+
+      rooms[room].push(socket.id);
+
+      // write in redis
+      client.get(room, (err, reply) => {
+        if (err) {
+          return;
+        }
+
+        // if key does not exist, reply is null
+        if (reply && reply == 1) {
+          client.set(room, 2);
+        } else {
+          client.set(room, 1);
+        }
+      });
+
+      console.log('socket id: ', socket.id);
+      console.log('join the room: ', rooms[room]);
+    });
+
+    /**
+     * Send message
+     *  Params:
+     *   - data: {from: 'FromAlias', to: 'ToAlias', message: 'messages'}  
+     */
+    socket.on(Send_Message, (data) => {
+      client.get(room, (err, reply) => {
+        if (reply && reply == 1) {
+          // TODO: push message
+
+        } else if (reply && reply == 2) {
+          Chat.saveChatMessage({
+            r_id: room,
+            from: data.from,
+            to: data.to,
+            message: data.message
+          }, (err, result) => {
+            if (err) {
+              console.error('saveChatMessage error:', err.stack);
+              socket.emit('foo', 'Server error');
+              return;
+            }
+
+            socket.broadcast.to(room).emit(Send_Message, data.message);
+          });
+        }
+      });
     });
 
     socket.on('disconnect', () => {
-      // TODO: disconnect 시 어떻게???
-      console.log('user disconnected');
+      if (room != undefined && rooms[room] != undefined && rooms[room].length > 0) {
+        for (let i = rooms[room].length - 1; i >= 0; i--) {
+          if (rooms[room][i] == socket.id) {
+            rooms[room].splice(i, 1);
+            client.get(room, (err, reply) => {
+              client.set(room, reply - 1);
+            });
+            break;
+          }
+        }
+      }
     });
 
   });
